a05975c9b59b4facd43b5f7a1190f68c972cf964
HelenaOstlund <ostlund_helena@hotmail.com>
2022-11-16 09:14:13 +0100

    initial commit

---
+++ src/main/java/com/team3/GruppArbeteWebbService/GruppArbeteWebbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team3.GruppArbeteWebbService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppArbeteWebbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppArbeteWebbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/GruppArbeteWebbService/GruppArbeteWebbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.GruppArbeteWebbService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppArbeteWebbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
